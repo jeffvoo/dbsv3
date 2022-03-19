f8db67c140d365e2b521db2d2708b9ceb6107a0e
3inyi16 <101925160+3inyi16@users.noreply.github.com>
2022-03-19 20:02:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
-from flask import Flask, request, render_template
+from flask import Flask
+app=Flask(__name__)
+
+from flask import request, render_template
 import joblib
 
-app = Flask(__name__) #__xx__
-
-#which directory is file in
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST": #when user press 'Enter' in front end
-        rates = request.form.get("rates")
+    if request.method == "POST":
+        rates=request.form.get("rates")
         print(rates)
         model = joblib.load('DBS_regression')
         pred = model.predict([[float(rates)]])
         print(pred)
-        s = "Predicted DBS Share Price : " + str(pred[0][0])
+        s = "Predicted DBS Share Price: " + str(pred)
         print(s)
         return(render_template("index.html", results=s))
 
-    else: #before user press 'Enter' in front end
+    else:
         return(render_template("index.html", results="DBS Share Price Prediction"))
 
 
-if __name__ == "__main__": #need this to run in cloud environment, to verify that code is mine
+if __name__=="__main__":
     app.run()
     
 
